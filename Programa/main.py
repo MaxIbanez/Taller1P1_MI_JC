7a97950c175672e13a5ce0b6f12525ed5ac21525
MaxIbanez <m.ibaezmolina@uandresbello.edu>
2020-08-28 19:09:34 -0400

    update diagFlujoListPersona

---
+++ Programa/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+class ProgramImc:
+    def __init__(self):
+        self.data = open("data","+w")
+    def CalculoImc(self):
+        alt = 0
+        peso = 0
+        return (alt/peso)
